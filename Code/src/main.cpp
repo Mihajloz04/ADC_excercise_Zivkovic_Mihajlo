daf1f4b359acd2d3861fb3c735b3d7b4cfff2976
Mihajloz04 <190045@htlw10.at>
2025-04-25 23:24:44 +0200

    Yes

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,22 +1,28 @@
-/**Name: Zivkovic Mihajlo
+/**
+ * Name: Zivkovic Mihajlo
  * Class: 4BHEL
  * Date:  24.04.2025
- * Project: ADC_Excercise
+ * Project: ADC_Exercise
  */
 #include <Arduino.h>
 
-const int poti=A0;
+const int poti = A0;
+unsigned long lastMillis = 0;
+const unsigned long interval = 500;
 
-void setup()
-{
-  Serial.begin
+void setup() {
+  Serial.begin(9600);
 }
-void loop()
-{
-  int AnalogWert= analogRead(poti);
-  float Spginput =(AnalogWert/1023.0)*5;
-  Serial.print("Potentiometer Wert = ");
-  Serial.print(Spginput);
-  Serial.print("V");
-  delay(500);
+
+void loop() {
+  if (millis() - lastMillis >= interval) {
+    lastMillis = millis();
+
+    int AnalogWert = analogRead(poti);
+    float Spginput = (AnalogWert / 1023.0) * 5.0;
+
+    Serial.print("Potentiometer Wert = ");
+    Serial.print(Spginput);
+    Serial.println(" V");
+  }
 }
