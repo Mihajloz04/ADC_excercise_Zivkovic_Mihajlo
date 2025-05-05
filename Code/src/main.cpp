9a409e3bdcef56d3773ef1991643766236aa3d31
Mihajloz04 <190045@htlw10.at>
2025-05-05 12:27:51 +0200

    Update main.cpp

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,28 +1,34 @@
 /**
- * Name: Zivkovic Mihajlo
- * Class: 4BHEL
- * Date:  24.04.2025
- * Project: ADC_Exercise
- */
+  Name: Zivkovic Mihajlo
+  Klasse: 4BHEL
+  Datum: 24.04.2025
+  Project: ADC_Exercise
+ **/
 #include <Arduino.h>
 
-const int poti = A0;
+const int poti = A0; 
 unsigned long lastMillis = 0;
 const unsigned long interval = 500;
 
-void setup() {
-  Serial.begin(9600);
-}
+void setup() 
+  {
+    Serial.begin(115200);
+    // ADC initialisieren
+    ADMUX |= (1 << REFS0);             
+    ADCSRA |= (1 << ADEN) |= (1 << ADPS2) | (1 << ADPS1)| (1 << ADPS0); 
+  }
 
 void loop() {
-  if (millis() - lastMillis >= interval) {
+  if (millis() - lastMillis >= interval) 
+  {
     lastMillis = millis();
-
-    int AnalogWert = analogRead(poti);
-    float Spginput = (AnalogWert / 1023.0) * 5.0;
-
+    ADCSRA |= (1 << ADSC);  
+    while (ADCSRA & (1 << ADSC)!=0);  
+    int analogWert = ADC;  
+    float spannung = (analogWert / 1023.0) * 5.0;
     Serial.print("Potentiometer Wert = ");
-    Serial.print(Spginput);
+    Serial.print(spannung);
     Serial.println(" V");
   }
 }
+/** Refferenzspannung, Prescaler, MUX, Start conmverstion bit und Interrupt  */
