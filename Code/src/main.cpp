03890b8d20327a534a50881490d9bf9a2a822e86
Mihajloz04 <190045@htlw10.at>
2025-05-12 13:21:30 +0200

    Update main.cpp

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -6,29 +6,37 @@
  **/
 #include <Arduino.h>
 
-const int poti = A0; 
+const int poti = A0;
+volatile int analogWert = 0;
+volatile bool adcReady = false;
+
 unsigned long lastMillis = 0;
 const unsigned long interval = 500;
 
-void setup() 
-  {
-    Serial.begin(115200);
-    // ADC initialisieren
-    ADMUX |= (1 << REFS0);             
-    ADCSRA |= (1 << ADEN) |= (1 << ADPS2) | (1 << ADPS1)| (1 << ADPS0); 
-  }
+void setup() {
+  Serial.begin(115200);
+  ADMUX = (1 << REFS0);
+  ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  sei();
+}
 
 void loop() {
-  if (millis() - lastMillis >= interval) 
-  {
+  if (millis() - lastMillis >= interval) {
     lastMillis = millis();
-    ADCSRA |= (1 << ADSC);  
-    while (ADCSRA & (1 << ADSC)!=0);  
-    int analogWert = ADC;  
+    ADCSRA |= (1 << ADSC);
+  }
+
+  if (adcReady) {
+    adcReady = false;
     float spannung = (analogWert / 1023.0) * 5.0;
     Serial.print("Potentiometer Wert = ");
-    Serial.print(spannung);
+    Serial.print(spannung, 3);
     Serial.println(" V");
   }
+}
+
+ISR(ADC_vect) {
+  analogWert = ADC;
+  adcReady = true;
 }
 /** Refferenzspannung, Prescaler, MUX, Start conmverstion bit und Interrupt  */
